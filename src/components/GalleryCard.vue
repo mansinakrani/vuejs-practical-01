f30966d626ea091e0e8a460eabb21963fb54853a
mansinakrani <mansinakrani006@gmail.com>
2022-03-24 15:02:34 +0530

    Created GalleryCard component

---
+++ src/components/GalleryCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GalleryCard',
+ data() {
+    return {
+      title: "Car Showroom",
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
